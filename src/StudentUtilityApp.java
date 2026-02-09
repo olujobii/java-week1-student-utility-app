4dde86a9a454622496a5c2d3d30572c632e253c4
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-09 05:45:24 +0100

    Refactoring and creating methods for each block of code

--- src/StudentUtilityApp.java
+++ src/StudentUtilityApp.java
@@ -16,94 +16,22 @@ public class StudentUtilityApp {
             System.out.print("Enter your choice: ");
 
             String userOption = scanner.nextLine();
+            //SWITCH STATEMENT TO EXECUTE BLOCK OF CODE BASED ON USER'S SELECTION.
             switch(userOption){
-                //ADDING TWO NUMBERS
                 case "1":
-                    boolean isFirstIntValid = false;
-                    int a  = 0 , b = 0;
-                    do{
-                    System.out.print("Enter first number: ");
-                    if(scanner.hasNextInt()){
-                       a = scanner.nextInt();
-                       scanner.nextLine();
-                       isFirstIntValid = true;
-                    }
-                    else{
-                        System.out.println("Not a valid integer");
-                        scanner.nextLine();
-                    }
-                    } while(!isFirstIntValid);
-
-                    boolean isSecondIntValid = false;
-                    do{
-                        System.out.print("Enter second number: ");
-                        if(scanner.hasNextInt()){
-                            b = scanner.nextInt();
-                            scanner.nextLine();
-                            isSecondIntValid = true;
-                        }
-                        else{
-                            System.out.println("Not a valid integer");
-                            scanner.nextLine();
-                        }
-                    } while(!isSecondIntValid);
-
-                    int result = addTwoNumbers(a,b);
-                    System.out.println("Result: "+result);
+                   addingTwoNumbers();
                     break;
                 //CHECKING IF NUMBER IS EVEN OR ODD
                 case "2":
-                    boolean isNumberValid = false;
-                    do{
-                        System.out.print("Enter a number: ");
-                        if(scanner.hasNextInt()){
-                            int userInput = scanner.nextInt();
-                            scanner.nextLine();
-                            System.out.println(isEvenOrOdd(userInput));
-                            isNumberValid = true;
-                        }else{
-                            System.out.println("Not a valid integer");
-                            scanner.nextLine();
-                        }
-                    }while (!isNumberValid);
+                   checkIfNumberIsEvenOrOdd();
                     break;
                 //PRINT NUMBER FROM 1 TO N
                 case "3":
-                    boolean isNumberPrinted = false;
-                    do{
-                        System.out.print("Enter a number: ");
-                        if(!scanner.hasNextInt()){
-                            System.out.println("Not a valid integer, try again");
-                            scanner.nextLine();
-                            continue;
-                        }
-
-                        int num = scanner.nextInt();
-                        scanner.nextLine();
-                        if(num <= 1){
-                            System.out.println("Your value cannot be less than or equal to 1");
-                        }else{
-                            printNumbers(num);
-                            isNumberPrinted = true;
-                        }
-                    }while(!isNumberPrinted);
+                    printNumbersFrom1ToN();
                     break;
                 //DISPLAYING DAYS OF THE WEEK
                 case "4":
-                    boolean isDaysOfWeekPrinted = false;
-                    do {
-                        System.out.print("Enter a number to display the corresponding day of the week: ");
-                        if (!scanner.hasNextInt()) {
-                            System.out.println("Not a valid integer, try again");
-                            scanner.nextLine();
-                            continue;
-                        }
-
-                        int num = scanner.nextInt();
-                        scanner.nextLine();
-                        isDaysOfWeekPrinted = printDayOfWeek(num);
-
-                    }while (!isDaysOfWeekPrinted);
+                    printDayOfWeek();
                     break;
                 //EXIT PROGRAM
                 case "5":
@@ -117,21 +45,117 @@ public class StudentUtilityApp {
         }
     }
 
+    //OPTION TO ADD TWO NUMBERS
+    static void addingTwoNumbers(){
+        boolean isUserInputValid = false;
+        int a  = 0 , b = 0;
+        do{
+            System.out.print("Enter first number: ");
+            if(scanner.hasNextInt()){ //Checking if user's input is a valid integer
+                a = scanner.nextInt();
+                scanner.nextLine();
+                isUserInputValid = true;
+            }
+            else{
+                System.out.println("Not a valid integer");
+                scanner.nextLine();
+            }
+        } while(!isUserInputValid);
+
+         isUserInputValid = false;
+        do{
+            System.out.print("Enter second number: ");
+            if(scanner.hasNextInt()){ //Checking if user's input is a valid integer
+                b = scanner.nextInt();
+                scanner.nextLine();
+                isUserInputValid = true;
+            }
+            else{
+                System.out.println("Not a valid integer");
+                scanner.nextLine();
+            }
+        } while(!isUserInputValid);
+
+        int result = addTwoNumbers(a,b);
+        System.out.println("Result: "+result);
+    }
+
+    //PERFORMING ARITHMETIC OPERATION
     private static int addTwoNumbers(int a, int b) {
         return a + b;
     }
 
-    static String isEvenOrOdd(int num){
+    //OPTION TO CHECK IF USER INPUT IS EVEN OR ODD
+    static void checkIfNumberIsEvenOrOdd(){
+        boolean isNumberValid = false;
+        do{
+            System.out.print("Enter a number: ");
+            if(scanner.hasNextInt()){ //Checking if user's input is a valid integer
+                int userInput = scanner.nextInt();
+                scanner.nextLine();
+                String result = isEvenOrOdd(userInput);
+                System.out.println(result);
+                isNumberValid = true;
+            }else{
+                System.out.println("Not a valid integer");
+                scanner.nextLine();
+            }
+        }while (!isNumberValid);
+    }
+
+    //CONDITIONAL CHECK TO SEE IF NUMBER IS EVEN OR ODD
+   private static String isEvenOrOdd(int num){
         return num % 2 == 0 ? "EVEN" : "ODD";
     }
 
-    static void printNumbers(int num){
+    //OPTION TO PRINT NUMBERS FROM 1 TO N
+    static void printNumbersFrom1ToN(){
+        boolean isNumberPrinted = false;
+        do{
+            System.out.print("Enter a number: ");
+            if(!scanner.hasNextInt()){ //Checking if user's input is a valid integer
+                System.out.println("Not a valid integer, try again");
+                scanner.nextLine();
+                continue;
+            }
+
+            int num = scanner.nextInt();
+            scanner.nextLine();
+            if(num <= 1){
+                System.out.println("Your value cannot be less than or equal to 1");
+            }else{
+                printNumbers(num);
+                isNumberPrinted = true;
+            }
+        }while(!isNumberPrinted);
+    }
+
+    private static void printNumbers(int num){
         for(int i = 1 ; i <= num ; i++){
             System.out.println(i);
         }
     }
 
-    static boolean printDayOfWeek(int num){
+    //OPTION TO PRINT DAY OF THE WEEK BASED ON USER'S INPUT
+    static void printDayOfWeek(){
+        boolean isDaysOfWeekPrinted = false;
+        do {
+            System.out.print("Enter a number to display the corresponding day of the week: ");
+            if (!scanner.hasNextInt()) { //Checking if user's input is a valid integer
+                System.out.println("Not a valid integer, try again");
+                scanner.nextLine();
+                continue;
+            }
+
+            int num = scanner.nextInt();
+            scanner.nextLine();
+            isDaysOfWeekPrinted = isDayOfWeekValid(num);
+
+        }while (!isDaysOfWeekPrinted);
+    }
+
+    //CONDITIONAL CHECK TO SEE IF USER'S INPUT IS VALID FOR DAY OF THE WEEK
+   private  static boolean isDayOfWeekValid(int num){
         switch(num){
             case 1:
                 System.out.println("Monday");
@@ -160,6 +184,7 @@ public class StudentUtilityApp {
         }
     }
 
+    //EXITING APPLICATION AND CLOSING SCANNER
     static void exitApplication(){
         System.out.println("Thank you for using the app");
         scanner.close();
